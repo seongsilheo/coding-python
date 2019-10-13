9acaf4e3f8592b67f405a2e94f509da5be184798
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2019-10-13 15:22:04 +0900

    Create dual_priority_queue.py

---
+++ dual_priority_queue.py
@@ -0,0 +1,43 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42628
+
+## 1
+def solution(operations):
+    queue = []
+    for i in operations:
+        op, num = i.split()
+        if op == "I":
+            queue.append(int(num))
+        elif op == "D" and queue:
+            if num == "-1":
+                queue.remove(min(queue))
+            elif num == "1":           
+                queue.remove(max(queue))
+
+    if queue:
+        return [max(queue), min(queue)]
+    else:
+        return [0,0]
+    
+    
+    
+    
+
+## 2
+import heapq
+
+def solution(operations):
+    queue = []
+    
+    for i in operations:
+        op, num = i.split()
+        if op == "I":
+            heapq.heappush(queue ,int(num))
+        elif op == "D" and queue:
+            if num == "-1":
+                 heapq.heappop(queue)
+            elif num == "1":
+                queue.pop(queue.index(max(queue)))               
+
+    if queue:
+        return [max(queue), heapq.heappop(queue)]
+    else:
